04005c536c23fdb64d791bce8da3ac5d4423fbe9
Perrupato <56273340+Perrupato@users.noreply.github.com>
2019-10-21 19:32:56 +0200

    Update MiNombre.cs

--- Unity/Assets/MiNombre.cs
+++ Unity/Assets/MiNombre.cs
@@ -6,17 +6,15 @@ public class MiNombre : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		void Update () {
-			void Start () {
-				Debug.Log ("Christian Gamero González"); 
-			}
+		Debug.Log ("Christian Gamero González"); 
 	}
+			
+
 	
 	// Update is called once per frame
 	void Update () {
-			void Update () {
-				void Start () {
-					Debug.Log ("20"); 
-				}	
-	}
+		Debug.Log ("20"); 
+	}	
+					
+	
 }
