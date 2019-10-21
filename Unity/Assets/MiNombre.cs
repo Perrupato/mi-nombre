5512bf53c59de514ac9ce9bee7d90124c1bda12a
Perrupato <christiangamero@ceitartessos.com>
2019-10-21 19:05:02 +0200

    Proyecto mi nombre

---
+++ Unity/Assets/MiNombre.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiNombre : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		void Update () {
+			void Start () {
+				Debug.Log ("Christian Gamero González"); 
+			}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			void Update () {
+				void Start () {
+					Debug.Log ("20"); 
+				}	
+	}
+}
